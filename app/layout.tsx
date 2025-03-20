994cd703f8aabd3bee6857b5714b0fbe04f30726
jessasesh <jessica.dison@yahoo.com>
2025-03-20 03:37:07 -0500

    App behavior

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,29 @@
-import "@/app/global.css";
-import { Metadata } from "next";
+import { ReactNode } from "react";
+import Providers from "./Providers";
+import Header from "../components/Header";
+import Sidebar from "../components/Sidebar";
+import "./global.css";
 
-export const metadata: Metadata = {
-  title: "Cinema Guru | Atlas School",
-};
-
-type Props = {
-  children: React.ReactNode;
-};
-
-export default function RootLayout({ children }: Props) {
+export default function RootLayout({ children }: { children: ReactNode }) {
   return (
-    <html lang="en">
-      <body className={`antialiased  bg-[#00003c] text-white`}>{children}</body>
+    <html lang="en" className="h-full overflow-hidden">
+      <body className="h-full flex flex-col overflow-hidden">
+        <Providers>
+          <Header />
+
+          <div className="flex flex-grow min-h-screen">
+            <Sidebar />
+            <main
+              id="main-content"
+              className="flex-grow min-h-[calc(100vh-8dvh)] overflow-y-auto bg-deepNavy p-6 ml-20 transition-all duration-300 md:ml-64"
+              role="main"
+              aria-labelledby="page-title"
+            >
+              {children}
+            </main>
+          </div>
+        </Providers>
+      </body>
     </html>
   );
 }
