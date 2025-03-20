3c2112f86ca7b62a50597d050f9782d5f70265dc
jessasesh <jessica.dison@yahoo.com>
2025-03-20 03:31:54 -0500

    GitHub auth setup

--- auth.ts
+++ auth.ts
@@ -1,4 +1,5 @@
 import NextAuth from "next-auth";
+import GitHubProvider from "next-auth/providers/github";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   theme: {
@@ -6,11 +7,22 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
     logo: "/logo.png",
     buttonText: "#ffffff",
   },
-  providers: [],
+  providers: [
+    GitHubProvider({
+      clientId: process.env.GITHUB_CLIENT_ID!,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET!,
+      authorization: {
+        params: {
+          prompt: "consent",
+          access_type: "offline",
+          response_type: "code",
+        },
+      },
+    }),
+  ],
   callbacks: {
-    authorized: async ({ auth }) => {
-      // Logged in users are authenticated, otherwise redirect to login page
-      return !!auth;
+    signIn: async ({ user, account, profile, email, credentials }) => {
+      return true;
     },
   },
 });
