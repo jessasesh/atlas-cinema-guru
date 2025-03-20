789f63b1d0f6ba5b3a627adee638f6c995a26053
jessasesh <jessica.dison@yahoo.com>
2025-03-20 03:31:16 -0500

    Dependencies and configs

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./app/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        darkBlue: "#121826",
+        Teal: "#1ED2AF",
+      },
+    },
+  },
+  plugins: [],
+};
